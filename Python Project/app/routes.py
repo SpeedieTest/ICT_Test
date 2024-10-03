aacf14fcb2f5eda0485eff99302082a72738db7b
FrogOverlord11 <jake@iveybros.net>
2024-10-03 19:05:07 +1000

    updates to routes.py
    keep old_routes.py just in case
    but other than that i should be done with routes.py

--- Python Project/app/routes.py
+++ Python Project/app/routes.py
@@ -4,6 +4,10 @@ from datetime import datetime
 from .sshlogin import generate_synthetic_logs as ssh_gsl
 from .sshlogin import save_logs as ssh_sl
 from .sshlogin import generate_daily_activity_logs as ssh_dal
+from .netflow import generate_synthetic_logs as netflow_gsl
+from .netflow import save_logs as netflow_sl
+from .netflow import generate_daily_activity_logs as netflow_dal
+
 from .value_generator import generate_random_username, generate_random_hostname, generate_random_source_path, generate_random_destination_path
 
 @app.route('/')
@@ -26,10 +30,12 @@ def submit_form():
     # Define a dictionary that maps log_type to the respective function calls
     manual_gen_switch = {
         'option1': handle_ssh_logs,
+        'option6': handle_netflow_logs,
     }
 
     quick_gen_switch = {
         'option1': lambda: handle_quick_gen('option1'),
+        'option6': lambda: handle_quick_gen('option6'),
     }
 
     # Handle manual generation based on log type
@@ -51,6 +57,7 @@ def handle_quick_gen(log_type):
     # Define a dictionary that maps log_type to the respective quick generation function
     quick_gen_switch = {
         'option1': generate_ssh_logs_quick,
+        'option6': generate_netflow_logs_quick,
     }
 
     # Use the log_type as a key to call the respective function
@@ -93,8 +100,40 @@ def handle_ssh_logs(request):
 
     return "SSH Logs generated successfully!"
 
+def handle_netflow_logs(request):
+    timestamp_str = request.form.get('netflow_timestamp')
+    source_ip = request.form.get('netflow_sourceip')
+    destination_ip = request.form.get('netflow_destinationip')
+    source_port = request.form.get('netflow_sourceport')
+    destination_port = request.form.get('netflow_destinationport')
+    no_connections = request.form.get('netflow_numberofconnections')
+    time_period = request.form.get('netflow_timeperiodinminutes')
+
+    #speedie needs to convert from sting to
+    #mine should work but no quarantee
+    #if not working look at line 86 "validate number of logs"
+
+    # Parse timestamp
+    timestamp = parse_timestamp(timestamp_str)
+    if not start_timestamp:
+        return "Error: Invalid timestamp format.", 400
+
+    # Generate and save SSH logs
+    logs = netflow_gsl(timestamp, source_ip, destination_ip, source_port, destination_port, no_connections, time_period)
+    netflow_sl(logs)
+    return "Netflow Logs generated successfully!"
+
+
+
+
+
 # Function to handle quick generation of SSH logs
 def generate_ssh_logs_quick():
     logs = ssh_dal()  # Call the function that generates daily SSH activity logs
     ssh_sl(logs)  # Save the logs
+    return "Daily network activity logs generated successfully!"
+
+def generate_netflow_logs_quick():
+    logs = netflow_dal()  # Call the function that generates daily SSH activity logs
+    netflow_sl(logs)  # Save the logs
     return "Daily network activity logs generated successfully!"
