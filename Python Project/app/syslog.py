1d56b155f29d070e0d3717e429272c36ac918521
FrogOverlord11 <jake@iveybros.net>
2024-10-09 16:59:20 +1100

    quality checks

--- Python Project/app/syslog.py
+++ Python Project/app/syslog.py
@@ -1,21 +1,53 @@
 import os
 import random
-from syslog_routes import num_logs
 from datetime import datetime, timedelta
-from .value_generator import (generate_random_username, generate_random_hostname, generate_random_event_outcome, generate_random_timestamp)
+from .value_generator import (generate_random_username, generate_random_hostname, generate_random_timestamp, generate_random_filename())
 
 # Function that generates logs
-def generate_synthetic_logs(timestamp_, host_name, user_name, file_path, file_name, bash_id):
+def generate_synthetic_logs(timestamp, host_name, user_name, file_path, file_name):
         logs = []
-        for i in range(num_logs):
-            formatted_timestamp = timestamp_.strftime('%b %d %H:%M:%S')
+        
+        formatted_timestamp = timestamp.strftime('%b %d %H:%M:%S')
             
-            bash_id = random.randint(1,65535)
+        bash_id = random.randint(10000, 99999)
             
-            log = f"{formatted_timestamp} {host_name} bash[{bash_id}]: {file_path} {file_name} executed by {user_name}"
-            
-        return log
+        log = f"{formatted_timestamp} {host_name} bash[{bash_id}]: {file_path} {file_name} executed by {user_name}"
+
+        logs.append(log)
+
+        return logs
     
+def generate_daily_activity_logs():
+    logs = []
+
+    for _ in range(20):
+        user = generate_random_username()
+        for _ in range(10):
+            login_time = generate_random_timestamp()
+            host_name = generate_random_hostname()
+            file_path = generate_random_filename()
+            file_name = 
+            user_name = 
+
+            logs.extend(generate_synthetic_logs(login_time, host_name, user_name, file_path, file_name))
+
+        
+        #malicious variable what is considered malicious (this generates the attack)
+        login_time = generate_random_timestamp()
+        host_name = generate_random_hostname()
+        file_path = generate_random_filename()
+        file_name = 
+        user_name = 
+
+        logs.extend(generate_synthetic_logs(login_time, host_name, user_name, file_path, file_name))
+    
+    # Sort logs by timestamp
+    logs.sort(key=lambda x: x[0])
+
+    # Extract just the log messages, discarding the timestamp
+    return [log for _, log in logs]
+
+
 # Function to save logs into a single file
 def save_logs(logs):
     os.makedirs('logs', exist_ok=True)
