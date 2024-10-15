a5439e7aef2b05fd4d37a651e957fca3bb4eb605
rash-mikka <rashmikka.t@gmail.com>
2024-10-15 13:40:26 +1100

    Update routes.py

--- Python Project/app/routes.py
+++ Python Project/app/routes.py
@@ -3,8 +3,8 @@ from app import app
 from datetime import datetime
 # Import SSHlogin log creation.
 from .log_gen_ssh import generate_single_sshlog, save_ssh_logs, auto_generate_ssh_logs
-from .log_gen_ftp import generate_single_ftplog, save_ftp_logs
-from .log_gen_filesystem import generate_single_fslog, save_fs_logs
+from .log_gen_ftp import generate_single_ftplog, save_ftp_logs, auto_generate_ftp_logs
+from .log_gen_filesystem import generate_single_fslog, save_fs_logs, auto_generate_fs_logs
 @app.route('/')
 @app.route('/index')
 def index():
@@ -154,16 +154,14 @@ def generate_ssh_logs_quick():
 
 # Function to handle quick generation of FTP logs
 def generate_ftp_logs_quick():
-    # Generate daily FTP activity logs 
-    #(mass_download_chance, mass_exfiltration_chance)
-    logs = auto_generate_ftp_logs(0.1,0.8,0.2, 0.1)  
+    # Generate daily FTP activity logs #(mass_download_chance, mass_exfiltration_chance)
+    logs = auto_generate_ftp_logs(0.2,0.66)  
     save_ftp_logs(logs)  # Save the logs
     return "Daily file transfer logs generated successfully!"
 
 # Function to handle quick generation of File System logs
 def generate_fs_logs_quick():
-    # Generate daily File System activity logs 
-    #(chance_of_exfiltration)
-    logs = auto_generate_ssh_logs(0.1,0.8,0.2, 0.1)  
+    # Generate daily File System activity logs (chance_of_exfiltration)
+    logs = auto_generate_fs_logs(0.45)  
     save_fs_logs(logs)  # Save the logs
     return "Daily file system logs generated successfully!"
